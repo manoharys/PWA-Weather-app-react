b16bb0987434a34508b38eac599acbf4b7efde42
manohar <manumanohar1527@gmail.com>
2020-12-14 13:45:18 +0530

    fetching weather data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FetchWeather } from "./api/FetchWeather";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <h1>Weather App</h1>
     </div>
   );
 }
