d35c52af00d6a4a7b3aabf688a994577a1efadf1
manohar <manumanohar1527@gmail.com>
2020-12-15 21:03:53 +0530

    Rendering weather results to the UI

--- src/App.js
+++ src/App.js
@@ -21,13 +21,32 @@ const App = () => {
       <input
         type="text"
         className="search"
-        placeholder="Search..."
+        placeholder="Search Location..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
-
 export default App;
